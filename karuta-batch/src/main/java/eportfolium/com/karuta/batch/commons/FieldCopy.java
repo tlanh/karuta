8e86471b29b94887572d96f5bb9c63dfaff03ce3
Nobry OUK <o.p.nobry@gmail.com>
2020-04-27 12:34:45 -0400

    * Adding license

--- karuta-batch/src/main/java/eportfolium/com/karuta/batch/commons/FieldCopy.java
+++ karuta-batch/src/main/java/eportfolium/com/karuta/batch/commons/FieldCopy.java
@@ -1,3 +1,18 @@
+/* =======================================================
+	Copyright 2020 - ePortfolium - Licensed under the
+	Educational Community License, Version 2.0 (the "License"); you may
+	not use this file except in compliance with the License. You may
+	obtain a copy of the License at
+
+	http://www.osedu.org/licenses/ECL-2.0
+
+	Unless required by applicable law or agreed to in writing,
+	software distributed under the License is distributed on an "AS IS"
+	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+	or implied. See the License for the specific language governing
+	permissions and limitations under the License.
+   ======================================================= */
+
 // Based on a solution by Stephan Windmüller in http://tapestry.1045711.n5.nabble.com/Cross-Validation-in-dynamic-Forms-td2427275.html 
 // and Shing Hing Man in http://tapestry.1045711.n5.nabble.com/how-to-recordError-against-a-form-field-in-a-loop-td5719832.html .
 
