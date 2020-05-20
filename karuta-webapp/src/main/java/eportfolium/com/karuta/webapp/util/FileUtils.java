61bdf20e720b14f5c8d874993fa0612f9d3e97b0
Robin Dupret <robin.dupret@gmail.com>
2020-05-20 13:54:50 +0200

    Remove Elgg related stuff
    This seems to be no longer used by the frontend.

--- karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/util/FileUtils.java
+++ karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/util/FileUtils.java
@@ -15,11 +15,6 @@
 
 package eportfolium.com.karuta.webapp.util;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -109,66 +104,6 @@ public class FileUtils{
 
 		return contentType;
   }
-
-
-  public static String getHTTPQuery(String urlToRead) {
-      URL url;
-      HttpURLConnection conn;
-      BufferedReader rd;
-      String line;
-      String result = "";
-      try {
-         url = new URL(urlToRead);
-         conn = (HttpURLConnection) url.openConnection();
-         conn.setRequestMethod("GET");
-         rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-         while ((line = rd.readLine()) != null) {
-            result += line;
-         }
-         rd.close();
-      } catch (IOException e) {
-         e.printStackTrace();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-      return result;
-   }
-
-  public static String postHTTPQuery(String url, String urlParameters) throws Exception {
-
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		//add reuqest header
-
-
-
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		return response.toString();
-
-	}
 
 
 }
