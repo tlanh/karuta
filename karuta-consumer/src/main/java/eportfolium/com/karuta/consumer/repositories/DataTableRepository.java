efe037d5c6fd3b3bceddf3dbe555693b48f2587a
Robin Dupret <robin.dupret@gmail.com>
2020-05-25 13:18:06 +0200

    Remove useless classes

--- karuta-consumer/src/main/java/eportfolium/com/karuta/consumer/repositories/DataTableRepository.java
+++
@@ -1,9 +0,0 @@
-package eportfolium.com.karuta.consumer.repositories;
-
-import eportfolium.com.karuta.model.bean.DataTable;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataTableRepository extends CrudRepository<DataTable, String> {
-}
--- karuta-consumer/src/main/java/eportfolium/com/karuta/consumer/repositories/LogTableRepository.java
+++
@@ -1,9 +0,0 @@
-package eportfolium.com.karuta.consumer.repositories;
-
-import eportfolium.com.karuta.model.bean.LogTable;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogTableRepository extends CrudRepository<LogTable, Integer> {
-}
