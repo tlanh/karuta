14ebc785eb13b02b02f5baed732a29d79624b217
Robin Dupret <robin.dupret@gmail.com>
2020-06-11 09:27:50 +0200

    Properly avoid accessing URL while unauthenticated
    To respect the expected behavior, let's configure Spring Security
    to properly prevent unauthenticated people from accessing any URL
    apart from the login one.

---
+++ karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/component/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package eportfolium.com.karuta.webapp.component;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/credential/login")
+                    .permitAll()
+                .and()
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated();
+    }
+}
