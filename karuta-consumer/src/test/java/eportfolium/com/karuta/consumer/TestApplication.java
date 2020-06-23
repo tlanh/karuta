2dba8a89fdab60db13695d68cf242286637e1aae
Robin Dupret <robin.dupret@uphf.fr>
2020-06-23 14:40:29 +0200

    Rely on YAML configuration files for test

--- karuta-consumer/src/test/java/eportfolium/com/karuta/consumer/TestApplication.java
+++ karuta-consumer/src/test/java/eportfolium/com/karuta/consumer/TestApplication.java
@@ -3,11 +3,9 @@ package eportfolium.com.karuta.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EntityScan("eportfolium.com.karuta.model.bean")
-@PropertySource("classpath:application.properties")
 public class TestApplication {
 
     public static void main(String[] args) {
