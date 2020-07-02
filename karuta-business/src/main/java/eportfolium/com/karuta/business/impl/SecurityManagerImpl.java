bea449690ed12c2d829a714f771701f7c183e1e6
Robin Dupret <robin.dupret@uphf.fr>
2020-07-02 15:41:44 +0200

    Fix permission check for `removeUsers`
    The role name must be in capital letters and for a strange reason,
    we need to rely on `@P` to be able to properly rely on the method
    parameter inside the SpEL expression.

--- karuta-business/src/main/java/eportfolium/com/karuta/business/impl/SecurityManagerImpl.java
+++ karuta-business/src/main/java/eportfolium/com/karuta/business/impl/SecurityManagerImpl.java
@@ -37,6 +37,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -127,8 +128,8 @@ public class SecurityManagerImpl implements SecurityManager {
 	}
 
 	@Override
-	@PreAuthorize("hasRole('admin') or principal.id == #forUser")
-	public void removeUsers(Long forUser) {
+	@PreAuthorize("hasRole('ADMIN') or principal.id == #forUser")
+	public void removeUsers(@P("forUser") Long forUser) {
 		groupUserRepository.deleteAll(groupUserRepository.getByUser(forUser));
 		credentialRepository.deleteById(forUser);
 	}
