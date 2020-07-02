a185a850db4bd91a2c544100200788373ba1289f
Robin Dupret <robin.dupret@uphf.fr>
2020-07-02 15:17:28 +0200

    Enable Srping Security annotations at the webapp level

--- karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/rest/RestApplication.java
+++ karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/rest/RestApplication.java
@@ -20,11 +20,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "eportfolium.com.karuta")
 @EntityScan(basePackages= "eportfolium.com.karuta.model.bean")
 @EnableJpaRepositories(basePackages = "eportfolium.com.karuta.consumer.repositories")
+@EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true, securedEnabled = true)
 public class RestApplication {
 
 	public static void main(String[] args) {
