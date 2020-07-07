b33b4ad10d527763f5227b9295f109e904c23d71
Robin Dupret <robin.dupret@uphf.fr>
2020-07-07 17:02:41 +0200

    Remove XML configuration

--- karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/rest/RestApplication.java
+++ karuta-webapp/src/main/java/eportfolium/com/karuta/webapp/rest/RestApplication.java
@@ -18,6 +18,8 @@ package eportfolium.com.karuta.webapp.rest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -27,10 +29,14 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EntityScan(basePackages= "eportfolium.com.karuta.model.bean")
 @EnableJpaRepositories(basePackages = "eportfolium.com.karuta.consumer.repositories")
 @EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true, securedEnabled = true)
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RestApplication.class);
+	}
 }
